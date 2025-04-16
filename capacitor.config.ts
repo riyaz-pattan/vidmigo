525c5ae83f08c7e390225965d017159a5a3c1f5e
Pattan Riyaz <riyazpattan987@gmail.com>
2025-04-16 14:36:21 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,14 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.477935605aeb40c184ac381589a742ca',
-  appName: 'vidmigo',
+  appId: 'com.vidmigo.app',  // Updated appId
+  appName: 'Vidmigo',
   webDir: 'dist',
   server: {
     url: 'https://47793560-5aeb-40c1-84ac-381589a742ca.lovableproject.com?forceHideBadge=true',
     cleartext: true
   },
   plugins: {
-    // Configure permissions for Android
     Permissions: {
       permissions: [
         "android.permission.READ_EXTERNAL_STORAGE",
